83877f0a89c96ac176093a2d36a665c973d19628
Will S <fitnesswill13@gmail.com>
2022-10-04 20:56:42 -0700

    figured changing class by jquery

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -14,19 +14,42 @@ WHEN I refresh the page
 THEN the saved events persist*/
 
 //query the sections and establish variable I will need
-var today = moment().format("MM/DD/YYYY");
-var weekday = moment().weekday()
+var today = moment().format("dddd, MM/DD/YYYY");
 $("#currentDay").text(today);
-console.log(today);
+//console.log(startTime);
+//var am9 = 
 
+scheduleArr = ["#toDo9", "#toDo10", "#toDo11", "#toDo12", "#toDo13", "#toDo14", "#toDo15", "#toDo16", "#toDo17"]
+
+var currentHour = moment().format("H")
+
+/*for (i = 0; i < scheduleArr.length; i++){
+    if($(scheduleArr[i]).moment().format("H") === currentHour){
+        $(".hour").addClass("present").removeClass("future");
+    }
+    else if($(scheduleArr[i]).moment().format("H") > currentHour){
+        $(scheduleArr[i]).addClass("future").removeClass("past");
+    }
+    else{
+        $(scheduleArr[i]).addClass("present").removeClass("hour");
+    }
+}*/
+$(".hour").addClass("present").removeClass("future");
+
+console.log(currentHour)
 $("#currentDay").attr("font-weight", "bold")
+
+    
 
 //run a function that changes the class from past present and future based on the time
 //if 0000 to 9 all have class of future, 1700-2359 all have past 
 
-$(function (){
-    $(".hour").addClass("future").removeClass("hour");
-})
+//$(function (){
+
+
+    
+//})
+//var startTime = moment().format(09:00:);
 
 //figure out moment.js and displaying current day... changing on new day
 // need to wrap everything in function and continuously run moment every hour or so...
@@ -39,5 +62,5 @@ $(function (){
 //utilize a save block that will save information printed
 //event listener on save button  
 //save what information is printed aka item.val() within each time block so that is it always there unless deleted
-console.log(moment())
+
 
