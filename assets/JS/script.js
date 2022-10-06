ad87f09de64292ca8e3a5f64dccb0002dfca33a4
Will S <fitnesswill13@gmail.com>
2022-10-05 19:18:14 -0700

    finished color changing by hour

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -18,10 +18,109 @@ var today = moment().format("dddd, MM/DD/YYYY");
 $("#currentDay").text(today);
 //console.log(startTime);
 //var am9 = 
+var hoursArr = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+var space9 = $("#9");
+var space10 = $("#10");
+var space11 = $("#11");
+var space12 = $("#12");
+var space13 = $("#13");
+var space14 = $("#14");
+var space15 = $("#15");
+var space16 = $("#16");
+var space17 = $("#17");
 
-scheduleArr = ["#toDo9", "#toDo10", "#toDo11", "#toDo12", "#toDo13", "#toDo14", "#toDo15", "#toDo16", "#toDo17"]
 
-var currentHour = moment().format("H")
+console.log(moment(currentHour))
+
+
+var currentHour = moment().format("H")// 16,17 etc
+changeColor();
+function changeColor(){
+    if (currentHour < 9){
+        $(".description").attr("class", "future description col-10");
+    }
+    else if (currentHour === 9){
+        $("#9").attr("class", "present  col-10");
+        $(".description").attr("class", "future description col-10");
+    }
+    else if (currentHour === 10){
+        $("#9").attr("class", "past col-10");
+        $("#10").attr("class", "present col-10");
+        $(".description").attr("class", "future description col-10");
+    }
+     else if (currentHour === 11){
+        $("#9").attr("class", "past col-10");
+        $("#10").attr("class", "past col-10");
+        $("#11").attr("class", "present col-10");
+        $(".description").attr("class", "future description col-10");
+    }
+    else if (currentHour === 12){
+        $("#9").attr("class", "past col-10");
+        $("#10").attr("class", "past col-10");
+        $("#11").attr("class", "past col-10");
+        $("#12").attr("class", "present col-10");
+        $(".description").attr("class", "future description col-10");
+    }
+    else if (currentHour === 13){
+        $("#9").attr("class", "past col-10");
+        $("#10").attr("class", "past col-10");
+        $("#11").attr("class", "past col-10");
+        $("#12").attr("class", "past col-10");
+        $("#13").attr("class", "present col-10");
+        $(".description").attr("class", "future description col-10");
+    }
+    else if (currentHour ===  14){
+        $("#9").attr("class", "past col-10");
+        $("#10").attr("class", "past col-10");
+        $("#11").attr("class", "past col-10");
+        $("#12").attr("class", "past col-10");
+        $("#13").attr("class", "past col-10");
+        $("#14").attr("class", "present col-10");
+        $(".description").attr("class", "future description col-10");
+    }
+    else if (currentHour ===15){
+        $("#9").attr("class", "past col-10");
+        $("#10").attr("class", "past col-10");
+        $("#11").attr("class", "past col-10");
+        $("#12").attr("class", "past col-10");
+        $("#13").attr("class", "past col-10");
+        $("#14").attr("class", "past col-10");
+        $("#15").attr("class", "present col-10");
+        $(".description").attr("class", "future description col-10");
+    }
+    else if (currentHour === 16){
+        $("#9").attr("class", "past col-10");
+        $("#10").attr("class", "past col-10");
+        $("#11").attr("class", "past col-10");
+        $("#12").attr("class", "past col-10");
+        $("#13").attr("class", "past col-10");
+        $("#14").attr("class", "past col-10");
+        $("#15").attr("class", "past col-10");
+        $("#16").attr("class", "present col-10");
+        $(".description").attr("class", "future description col-10");
+    }
+    else if (currentHour === 17){
+        $("#9").attr("class", "past col-10");
+        $("#10").attr("class", "past col-10");
+        $("#11").attr("class", "past col-10");
+        $("#12").attr("class", "past col-10");
+        $("#13").attr("class", "past col-10");
+        $("#14").attr("class", "past col-10");
+        $("#15").attr("class", "past col-10");
+        $("#16").attr("class", "past col-10");
+        $("#17").attr("class", "present col-10");
+        $(".description").attr("class", "future description col-10");
+    }
+    else{
+        $(".description").attr("class", "past description col-10");
+    }
+        // everything before 13 past everything after 13 future and 13 = present
+    }
+
+
+
+    
+
 
 /*for (i = 0; i < scheduleArr.length; i++){
     if($(scheduleArr[i]).moment().format("H") === currentHour){
@@ -34,11 +133,16 @@ var currentHour = moment().format("H")
         $(scheduleArr[i]).addClass("present").removeClass("hour");
     }
 }*/
-$(".hour").addClass("present").removeClass("future");
+
 
 console.log(currentHour)
-$("#currentDay").attr("font-weight", "bold")
 
+
+
+$(".saveBtn9").on("submit", "#toDo9", function(e){
+    e.preventDefault();
+    var toDoList = localStorage.setItem("schedule9", JSON.stringify())
+})
     
 
 //run a function that changes the class from past present and future based on the time
